6d3b15967f81f38ca398005d590436e5a078c6d5
artie37 <artieknapp@dyn-169-226-47-165.wireless.albany.edu>
2014-03-05 14:19:14 -0500

    03/05/2014: First main file. Checks for the correct number of arguments and if both the config file and the query file open. If any of these cases fail, the program will print an error message and close.

--- main.c
+++ main.c
@@ -1,18 +1,39 @@
-//
-//  main.c
-//  Program_3
-//
-//  Created by artie37 on 3/5/14.
-//  Copyright (c) 2014 artie37. All rights reserved.
-//
+// Arthur Knapp
+// AK146316
+// main.c
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "constants.h"
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    FILE *configFile;
+    FILE *queryFile;
+    // Define file pointers for the config file and query file
+    
+    if (argc != ARGS)
+    // If the number of arguments is not 3, close program.
+    {
+        fprintf(stderr, "Error: Incorrect Number Of Arguments\n");
+        exit(1);
+    }
+    
+    if ((configFile = fopen(argv[FIRST_ARG], "r")) == NULL)
+    {
+        fprintf(stderr, "Error: Could Not Open configfile\n");
+        exit(1);
+    }
+    
+    if ((queryFile = fopen(argv[SECOND_ARG], "r")) == NULL)
+    {
+        fprintf(stderr, "Error: Could Not Open queryfile\n");
+        exit(1);
+    }
+    
+    
+    
     return 0;
 }
 
