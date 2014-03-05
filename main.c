e50870fbaf5abaa0033c5fe0e97d444932b800ce
artie37 <artieknapp@dyn-169-226-75-187.wireless.albany.edu>
2014-03-05 11:29:20 -0500

    03/05/2014: Test

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Program_3
+//
+//  Created by artie37 on 3/5/14.
+//  Copyright (c) 2014 artie37. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
