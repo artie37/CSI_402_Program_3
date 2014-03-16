772541801207d7a6c8428c5eabce06540cb9a748
artie37 <artieknapp@Arties-MacBook-Air.local>
2014-03-16 17:14:39 -0400

    03/16/2014: Header file to define all of the constants used in the program.

--- constants.h
+++ constants.h
@@ -3,5 +3,13 @@
 // constants.h
 
 #define ARGS 3
+// Used to check the number of command line arguments
 #define FIRST_ARG 1
+// Used to access index value 1 of the arguments array
 #define SECOND_ARG 2
+// Used to access index value 2 of the arguments array
+#define NAME_SIZE 16
+// Used to define the size of the name character array in
+// the relation and attribute name.
+#define STRUCT_SIZE 100
+// Used to define the size of both struct arrays
