741cab962322b8e3eb483ff9cfcfb9d222a8a503
artie37 <artieknapp@Arties-MacBook-Air.local>
2014-03-16 17:13:13 -0400

    03/16/2014: This is the header file to define all of the global variables.

---
+++ globals.h
@@ -0,0 +1,13 @@
+//
+//  globals.h
+//  Program_3
+//
+//  Created by artie37 on 3/14/14.
+//  Copyright (c) 2014 artie37. All rights reserved.
+//
+
+
+RelationsTable relTable[STRUCT_SIZE];
+// Define instance of relations table
+AttributesTable attrTable[STRUCT_SIZE];
+// Define instance of attributes table
---
+++ structs.h
@@ -0,0 +1,14 @@
+//
+//  structs.h
+//  Program_3
+//
+//  Created by artie37 on 3/14/14.
+//  Copyright (c) 2014 artie37. All rights reserved.
+//
+
+#ifndef Program_3_structs_h
+#define Program_3_structs_h
+
+
+
+#endif
