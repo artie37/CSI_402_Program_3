dca1723897c4f53fa9500fc5f6ec6896b1e30eaa
artie37 <artieknapp@dyn-169-226-220-247.wireless.albany.edu>
2014-03-20 10:48:18 -0400

    03/20/2014: File containing function prototypes.

--- prototypes.h
+++ prototypes.h
@@ -1,14 +1,7 @@
-//
-//  prototypes.h
-//  Program_3
-//
-//  Created by artie37 on 3/18/14.
-//  Copyright (c) 2014 artie37. All rights reserved.
-//
+// prototypes.h
+// Arthur Knapp
+// AK146316
 
-#ifndef Program_3_prototypes_h
-#define Program_3_prototypes_h
-
-
-
-#endif
+void numberOfAttributes(char *);
+void tupleLength(char *);
+void attributeInfo(char *, char *);
