ff11dd261ae6477d3c74ae1e9702d80de886ab9c
artie37 <artieknapp@dyn-169-226-220-247.wireless.albany.edu>
2014-03-20 12:46:37 -0400

    03/20/2014: Added the function numberOfTuples to calculate and print the number of tuples in a relation.

--- commands.c
+++ commands.c
@@ -2,25 +2,30 @@
 // Arthur Knapp
 // AK146316
 
-// This file contains the functions for processing the commands from the query file
+// This file contains the functions for processing the commands from the query file. Following is
+// information about each function.
 
 // Functions:
-// numberOfAttributes: This functions prints the number of attributes from a specified relation.
+
+// numberOfAttributes:
+// This functions prints the number of attributes from a specified relation.
 // It loops through the relation table comparing the relation from the query file with the relation
 // name in the relation struct array. If the relation is found, the number of attributes for that relation
-// is printed. There is a flag called "foundRel" that is by default set to 0 and is set to 1 if the
-// relation is found. If the flag is 0 after all of the relations are search through, the function
+// is printed. There is a flag called "foundRel" that is by default set to 0, and is set to 1 if the
+// relation is found. If the flag is 0 after all of the relations are searched through, the function
 // will print an error message stating that the relation was not found.
 
-// tupleLength: This function prints the length of each tuple from a specified relation.
+// tupleLength:
+// This function prints the length of each tuple from a specified relation.
 // It loops through the relation table comparing the relation from the query file with the relation
 // name in the relation struct array. If the relation is found, the length of the tuple will be calculated
 // by looping through the attributes table and finding the sum of all attribute sizes. After the
 // loop is completed, this value will be printed. There is a flag called "foundRel" that is by default
-// set to 0 and is set to 1 if the relation is found. If the flag is 0 after all of the relations
-// are search through, the function will print an error message stating that the relation was not found.
+// set to 0, and is set to 1 if the relation is found. If the flag is 0 after all of the relations
+// are searched through, the function will print an error message stating that the relation was not found.
 
-// attributeInfo: This function prints the type (int or string) and the length of the specified attribute.
+// attributeInfo:
+// This function prints the type (int or string) and the length of the specified attribute.
 // It loops through the relation table comparing the relation from the query file with the relation
 // name in the relation struct array. If the relation is found, the type and the length of the specified
 // attribute will be found by looping through the attributes table and finding the specified attribute.
@@ -29,12 +34,21 @@
 // specified are found, the flags will be set to 1. If they are not found, they will remain 0 and the
 // function will print an error message for which flag remains 0.
 
+// count:
+// This function will compute and print the number of tuples in the specified relation. To find this value,
+// the tuplen function will be called and will return the size of each tuple. After this value is found,
+// the size of the data file for the specified relation will be found. This value will be found by using
+// fseek to get to the end of the file and then using ftell to get the size. After the file size is found,
+// the size of the data file will be divided by the value returned by tuplen(length of each tuple). There
+// is a flag called "foundRel" that is by defaul set to 0, and is set to 1 if the relation is found. If
+// the flag is 0 after all of the relations are searched through, the function will print an error message
+// stating that the relation was not found.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "constants.h"
 #include "structs.h"
-//#include "globals.h"
 #include "externs.h"
 
 
@@ -65,7 +79,7 @@ void numberOfAttributes(char *arg1)
     }
 }
 
-void tupleLength(char *arg1)
+int tupleLength(char *arg1, int countFlag)
 {
     int i = 0;
     // Int variable to loop through relations table
@@ -93,7 +107,12 @@ void tupleLength(char *arg1)
                 tupleSize += attrTable[j].size;
             }
             
-            printf("Length Of Tuple: %d\n", tupleSize);
+            if (countFlag == 0)
+            // Checks to see if tuplen was called from the query file and prints tuple size
+            {
+                printf("Length Of Tuple: %d\n", tupleSize);
+            }
+            
             foundRel = 1;
             // Set foundRel to 1 to signify finding the relation
         }
@@ -103,6 +122,18 @@ void tupleLength(char *arg1)
     // Checks to see if the relation was not found and prints error message
     {
         printf("Error: Relation Not Found For tuplen\n");
+    }
+    
+    if (countFlag == 0)
+    // Checks if tuplen was called from the query file and returns zero
+    {
+        return 0;
+    }
+    
+    else
+    // If tuplen was called from numberOfTuples, return tuple size
+    {
+        return tupleSize;
     }
 }
 
@@ -159,4 +190,64 @@ void attributeInfo(char *arg1, char *arg2)
     {
         printf("Error: Attribute Not found\n");
     }
+}
+
+void numberOfTuples(char *arg1, int countFlag)
+{
+    int i = 0;
+    // Int variable to loop through the relations table
+    int foundRel = 0;
+    // Int variable to signify that the relation was found
+    int dataSize;
+    // Int variable to store the size of the data file
+    int tupleSize;
+    // Int variable to store the tuple size
+    int tupleCount;
+    // Int variable to store the number of tuples
+    FILE *dataFile;
+    // File pointer to store the data file
+    char datName[DATA_SIZE];
+    // Character array to store relation.dat file name
+    
+    tupleSize = tupleLength(arg1, countFlag);
+    
+    for (; i < relations; i++)
+    // Loops through relations table 'relations' times
+    {
+        if (strcmp(arg1, relTable[i].name) == 0)
+        // Checks if the specified relation is in the relation table and opens data file
+        {
+            strcpy(datName, relTable[i].name);
+            strcat(datName, ".dat");
+            // Insert the relation name into data name, then append
+            // '.dat' at the end for the name of the data file.
+            // This will be used to open the data file for the proper relation.
+            
+            if ((dataFile = fopen(datName, "r")) == NULL)
+            // If the data file doesn't open, close program
+            {
+                fprintf(stderr, "Error: Could Not Open Data File\n");
+                exit(1);
+            }
+            
+            foundRel = 1;
+            // Set foundRel to 1 to signify finding the relation
+        }
+    }
+    
+    if (foundRel == 0)
+        // Checks if foundRel wasn't found and prints error message
+    {
+        printf("Error: Relation Not Found\n");
+        return;
+    }
+
+    fseek(dataFile, 0L, SEEK_END);
+    // Set pointer to the end of the data file
+    dataSize = (int)ftell(dataFile);
+    // Insert the current position into dataSize
+    
+    tupleCount = dataSize / tupleSize;
+    
+    printf("Number Of Tuples: %d\n", tupleCount);
 }
