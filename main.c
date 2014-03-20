496b715f61c46d0f26cc0851156bab2a5cd7b882
artie37 <artieknapp@dyn-169-226-220-247.wireless.albany.edu>
2014-03-20 10:54:00 -0400

    03/20/2014: Add function calls to query functions.

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include "constants.h"
 #include "structs.h"
 #include "globals.h"
+#include "prototypes.h"
 
 int main(int argc, const char * argv[])
 {
@@ -17,8 +18,6 @@ int main(int argc, const char * argv[])
     FILE *schemaFile;
     FILE *dataFile;
     // Define file pointers for the schema file and data file
-    int relations;
-    // Declare int to store the number of relations
     int i;
     // Counter for relations for loop
     int j = 0;
@@ -31,9 +30,20 @@ int main(int argc, const char * argv[])
     // Character array to store relation.dat file name
     char schName[19];
     // Character array to store relation.sch file name
+    char command[COMMAND_SIZE];
+    // Character array to store a command from the query file
+    char arg1[ARG_SIZE];
+    // Character array for first argument from the query file
+    char arg2[ARG_SIZE];
+    // Character array for second argument from the query file
+    char arg3[ARG_SIZE];
+    // Character array for third argument from the query file
+    char arg4[ARG_SIZE];
+    // Character array for forth argument from the query file
+    int fscanfNum = 1;
     
     
-    //if (argc != ARGS)
+    if (argc != ARGS)
     // If the number of arguments is not 3, close program.
     if (argc != 2)
     {
@@ -48,12 +58,12 @@ int main(int argc, const char * argv[])
         exit(1);
     }
     
-//    if ((queryFile = fopen(argv[SECOND_ARG], "r")) == NULL)
-//    // If the query file dosen't open, close program
-//    {
-//        fprintf(stderr, "Error: Could Not Open Query File\n");
-//        exit(1);
-//    }
+    if ((queryFile = fopen(argv[SECOND_ARG], "r")) == NULL)
+    // If the query file dosen't open, close program
+    {
+        fprintf(stderr, "Error: Could Not Open Query File\n");
+        exit(1);
+    }
     
     fscanf(configFile, "%d", &relations);
     // Read the number of relations from the config file
@@ -110,34 +120,84 @@ int main(int argc, const char * argv[])
         }
     }
     
-    // Following code is for testing purposes. This code prints out both tables
-    // to test that the tables store the information properly.
-    i = 0;
-    j = 0;
-    tempOffset = 0;
-    
-    while (i < relations)
+    while (fscanf(queryFile, "%s ", command) == 1)
+    // Loop through querry file and run each command
     {
-        printf("Relations Table:\n"); fflush(stdout);
-        printf("Name: %s | ", relTable[i].name); fflush(stdout);
-        printf("Offset: %d | ", relTable[i].offset); fflush(stdout);
-        printf("NumAttr: %d\n", relTable[i].numAttr); fflush(stdout);
-        
-        printf("\n\n"); fflush(stdout);
-        printf("Attributes Table:\n"); fflush(stdout);
-        tempOffset = relTable[i].offset;
-        
-        while (j < relTable[i].numAttr + tempOffset)
+        if (strcmp(command, "nattr") == 0)
+        // Checks to see if the command is "nattr"
         {
-            printf("Name: %s | ", attrTable[j].name); fflush(stdout);
-            printf("Type: %c | ", attrTable[j].type); fflush(stdout);
-            printf("Size: %d\n", attrTable[j].size); fflush(stdout);
-            j++;
+            fscanf(queryFile, "%s", arg1);
+            numberOfAttributes(arg1);
         }
         
-        printf("-----------------------------------------------------------\n\n\n"); fflush(stdout);
-        i++;
+        if (strcmp(command, "tuplen") == 0)
+        // Checks to see if the command is "tuplen"
+        {
+            fscanf(queryFile, "%s", arg1);
+            tupleLength(arg1);
+        }
+        
+        if (strcmp(command, "infattr") == 0)
+        // Checks to see if the command is "infattr"
+        {
+            fscanf(queryFile, "%s ", arg1);
+            fscanf(queryFile, "%s", arg2);
+            attributeInfo(arg1, arg2);
+        }
+        
+        if (strcmp(command, "count") == 0)
+        // Checks to see if the command is "count"
+        {
+            
+        }
+        
+        if (strcmp(command, "project") == 0)
+        // Checks to see if the command is "project"
+        {
+            
+        }
+        
+        if (strcmp(command, "select") == 0)
+        // Checks to see if the command is "select
+        {
+            
+        }
+        
+        if (strcmp(command, "quit") == 0)
+        // Checks to see if the command is quit
+        {
+            exit(0);
+        }
     }
+    
+    // Following code is for testing purposes. This code prints out both tables
+    // to test that the tables store the information properly.
+//    i = 0;
+//    j = 0;
+//    tempOffset = 0;
+//    
+//    while (i < relations)
+//    {
+//        printf("Relations Table:\n"); fflush(stdout);
+//        printf("Name: %s | ", relTable[i].name); fflush(stdout);
+//        printf("Offset: %d | ", relTable[i].offset); fflush(stdout);
+//        printf("NumAttr: %d\n", relTable[i].numAttr); fflush(stdout);
+//        
+//        printf("\n\n"); fflush(stdout);
+//        printf("Attributes Table:\n"); fflush(stdout);
+//        tempOffset = relTable[i].offset;
+//        
+//        while (j < relTable[i].numAttr + tempOffset)
+//        {
+//            printf("Name: %s | ", attrTable[j].name); fflush(stdout);
+//            printf("Type: %c | ", attrTable[j].type); fflush(stdout);
+//            printf("Size: %d\n", attrTable[j].size); fflush(stdout);
+//            j++;
+//        }
+//        
+//        printf("-----------------------------------------------------------\n\n\n"); fflush(stdout);
+//        i++;
+//    }
     return 0;
 }
 
