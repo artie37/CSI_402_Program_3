069fc30836ee7e799d69ab08a0faafa4b7ab3891
artie37 <artieknapp@dyn-169-226-71-30.wireless.albany.edu>
2014-03-25 17:48:35 -0400

    03/25/2014: Updated externs header file.

--- externs.h
+++ externs.h
@@ -8,3 +8,7 @@ extern AttributesTable attrTable[STRUCT_SIZE];
 // Define instance of attributes table
 extern int relations;
 // Stores the number of relations to loop through the config file
+extern char *data;
+// Stores the string value from the data file
+extern int num;
+// Stores the int value from the data file
---
+++ linkedlist.c
@@ -0,0 +1,9 @@
+//
+//  linkedlist.c
+//  Program_3
+//
+//  Created by Arthur Knapp on 3/24/14.
+//  Copyright (c) 2014 artie37. All rights reserved.
+//
+
+#include <stdio.h>
