fafaad142387848746aa9a39203ff85b9852bb8d
artie37 <artieknapp@dyn-169-226-71-30.wireless.albany.edu>
2014-03-25 17:54:45 -0400

    03/25/2014: Added a structure definition for a linked list.

--- structs.h
+++ structs.h
@@ -2,6 +2,18 @@
 // AK146316
 // structs.h
 
+// Struct Descriptions:
+
+// RelationsTable:
+// Stores the name, offset and number of attributes from the relations file
+
+// AttributesTable:
+// Stores the name, type and size from the attributes file
+
+// LinkedList:
+// Stores the value from the data file of the attribute entered in the command line.
+// This will be used by the project and select commands.
+
 typedef struct RelationsTable
 {
     char name[NAME_SIZE];
@@ -15,3 +27,11 @@ typedef struct AttributesTable
     char type;
     int size;
 } AttributesTable;
+
+typedef struct LinkedList
+{
+    char *name;
+    int num;
+    
+    struct LinkedList *next;
+} Node;
