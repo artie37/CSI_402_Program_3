a05a55060026f7e501873a44c9d871af085b6c33
artie37 <artieknapp@dyn-169-226-71-30.wireless.albany.edu>
2014-03-25 17:54:15 -0400

    03/25/2014: This file contains the functions to run a linked list.

--- linkedlist.c
+++ linkedlist.c
@@ -1,9 +1,250 @@
-//
-//  linkedlist.c
-//  Program_3
-//
-//  Created by Arthur Knapp on 3/24/14.
-//  Copyright (c) 2014 artie37. All rights reserved.
-//
+// linked_list.c
+// Arthur Knapp
+// AK146316
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "constants.h"
+#include "structs.h"
+#include "externs.h"
+#include "prototypes.h"
+
+Node *pHead = NULL;
+Node *pTail = NULL;
+
+void LinkedListString(char *attrName)
+{
+    if (pHead == NULL)
+    // Checks to see if the head node is pointing to null and calls 'createNewList'.
+    // If the head node is pointing to null, the list is empty and a new list must be created.
+    {
+        createNewListString(attrName);
+    }
+    
+    else
+    // Runs if the list is not empty and inserts the new node at the end of the list
+    {
+        Node *pNewNode = malloc(sizeof(Node));
+        // Creates new node to be inserted into the list
+        Node *pCurr = pHead;
+        // Creates node to loop through linked list
+        pNewNode -> name = malloc(strlen(attrName));
+    
+        strcpy(pNewNode -> name, attrName);
+        // Insert the string into the new node
+        
+        if (pHead == pTail)
+        // Checks to see if the new node is going to be the second node in the list.
+        // If head and tail are the same, there is only one node.
+        {
+            if (strcmp(pHead -> name, attrName) == 0)
+            // If the string about to be inserted into the list equals the string in
+            // the head node, do nothing. This will eliminate duplicate entries.
+            {}
+            
+            else
+            // Inserts the new node if the current string does not equal to the head node
+            {
+                pHead -> next = pNewNode;
+                // Head node points to the new node
+                pTail = pNewNode;
+                // Insert the new node into tail
+                pNewNode -> next = NULL;
+                // Remove the new node from the list
+            }
+        }
+        
+        else
+        // If the new node is not the second node
+        {
+            while (pCurr != NULL)
+            // Loop through list to insert node
+            {
+                if (strcmp(pCurr -> name, attrName) == 0)
+                // If the string about to be inserted into the list equals the string in
+                // the current node, break out of loop. This will eliminate duplicate entries.
+                {
+                    break;
+                }
+                
+                else if (pCurr -> next == NULL)
+                // If the current node points to null, insert the current node
+                {
+                    pTail -> next = pNewNode;
+                    // Point the tail node to the new node
+                    pNewNode -> next = NULL;
+                    // Remove the new node from the list
+                    pTail = pNewNode;
+                    // Insert the new node into tail
+                }
+                
+                pCurr = pCurr -> next;
+                // Move to next node
+            }
+        }
+    }
+}
+
+void LinkedListInt(int attrNum)
+{
+    Node *pNewNode = malloc(sizeof(Node));
+    // Creates new node to be inserted into the list
+    Node *pCurr = pHead;
+    // Creates node to loop through linked list
+    
+    if (pHead == NULL)
+    // Checks to see if the head node is pointing to null and calls 'createNewList'.
+    // If the head node is pointing to null, the list is empty and a new list must be created.
+    {
+        createNewListInt(attrNum);
+    }
+    
+    else
+    // Runs if the list is not empty and inserts the new node at the end of the list
+    {
+        if (pHead == pTail)
+        // Checks to see if the new node is going to be the second node in the list.
+        // If head and tail are the same, there is only one node.
+        {
+            if (pHead -> num == attrNum)
+            // If the number about to be inserted into the list equals the number in
+            // the head node, do nothing. This will eliminate duplicate entries.
+            {}
+            
+            else
+            // Inserts the new node if the current string does not equal to the head node
+            {
+                pNewNode -> num = attrNum;
+                // Insert the number into the new node
+                pHead -> next = pNewNode;
+                // Head node points to the new node
+                pTail = pNewNode;
+                // Insert the new node into tail
+                pNewNode -> next = NULL;
+                // Remove the new node from the list
+            }
+        }
+        
+        else
+        // If the new node is not the second node
+        {
+            while (pCurr != NULL)
+            // Loop through list to insert node
+            {
+                if (pCurr -> num == attrNum)
+                // If the number about to be inserted into the list equals the number in
+                // the current node, break out of loop. This will eliminate duplicate entries.
+                {
+                    break;
+                }
+                
+                else if (pCurr -> next == NULL)
+                // If the current node points to null, insert the current node
+                {
+                    pNewNode -> num = attrNum;
+                    // Insert the number into the new node
+                    pTail -> next = pNewNode;
+                    // Point the tail node to the new node
+                    pNewNode -> next = NULL;
+                    // Remove the new node from the list
+                    pTail = pNewNode;
+                    // Insert the new node into tail
+                }
+                
+                pCurr = pCurr -> next;
+                // Move to next node
+            }
+        }
+    }
+}
+
+void createNewListString(char *attrName)
+{
+    Node *pNewNode = malloc(sizeof(Node));
+    // Creates new node to be inserted into the list
+    pNewNode -> next = NULL;
+    // Set the new nodes reference to null
+    pNewNode -> name = malloc(strlen(attrName));
+    
+    strcpy(pNewNode -> name, attrName);
+    // Insert the string into the new node
+    
+    pHead = pTail = pNewNode;
+    // Set the head and tail equal to the new node because
+    // there is only one node.
+}
+
+void createNewListInt(int attrNum)
+{
+    Node *pNewNode = malloc(sizeof(Node));
+    // Creates new node to be inserted into the list
+    
+    pNewNode -> next = NULL;
+    // Set the new nodes refence to null
+    
+    pNewNode -> num = attrNum;
+    // Insert the int into the new node
+    
+    pHead = pTail = pNewNode;
+    // Set the head and tail equal to the new node because
+    // there is only one node.
+}
+
+void printListString()
+{
+    Node *pCurr = pHead;
+    // Node pointer to loop through list
+    
+    if (pHead == NULL)
+    // If the list is empty, print error message
+    {
+        printf("Error: Nothing was read into the list\n"); fflush(stdout);
+    }
+    
+    else
+    // If the list is not empty
+    {
+        while (pCurr != NULL)
+        // Loop through the list and print values
+        {
+            printf("%s\n", pCurr -> name); fflush(stdout);
+            // Print the value in the current node
+            
+            pCurr = pCurr -> next;
+            // Move to next node
+        }
+        
+        pHead = NULL;
+    }
+}
+
+void printListInt()
+{
+    Node *pCurr = pHead;
+    // Node pointer to loop through list
+    
+    if (pHead == NULL)
+    // If the list is empty, print error message
+    {
+        printf("Error: Nothing was read into the list\n"); fflush(stdout);
+    }
+    
+    else
+    // If the list is not empty
+    {
+        while (pCurr != NULL)
+        // Loop through the list and print values
+        {
+            printf("%d\n", pCurr -> num); fflush(stdout);
+            // Print the value in the current node
+            
+            pCurr = pCurr -> next;
+            // Move to next node
+        }
+        
+        pHead = NULL;
+    }
+}
+
+
