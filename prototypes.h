cca474c94c74d477f4965bd0b48ff708a9fe89e0
artie37 <artieknapp@dyn-169-226-33-25.wireless.albany.edu>
2014-03-27 16:16:59 -0400

    03/27/2014: Updated prototypes header file.

--- prototypes.h
+++ prototypes.h
@@ -2,15 +2,18 @@
 // Arthur Knapp
 // AK146316
 
+// This function contains all of the function prototypes for the program
+
 void numberOfAttributes(char *);
 int tupleLength(char *, int);
 void attributeInfo(char *, char *);
 void numberOfTuples(char *, int);
 void project(char *, char *);
+void selectTuple(char *, char *, char *, char *);
 void insertString(char *);
 void insertInt(int);
-void LinkedListString(char *);
-void LinkedListInt(int);
+void linkedListString(char *);
+void linkedListInt(int);
 void createNewListString(char *);
 void createNewListInt(int);
 void printListString();
